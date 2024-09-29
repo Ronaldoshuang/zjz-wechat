8635f1f27c079fbb6da65013534ee163d636c406
卿爽 <541400574@qq.com>
2024-09-29 09:40:12 +0800

    添加客服

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ module.exports = {
     skipAd: false,
     host: {
         // photo: "https://ai-zjz.cn:8080" //线上直接访问后端
-        // photo: "https://ai-zjz.cn/api" ///线上nginx转发访问
+        photo: "https://ai-zjz.cn/api" ///线上nginx转发访问
 
-        photo: "http://127.0.0.1:8080" //本地服务访问
+        // photo: "http://127.0.0.1:8080" //本地服务访问
     }
     
 };
